60c13edfb8201f1919f6605eb95386adc408966b
Sickle41 <dracopest@gmail.com>
2026-05-04 17:07:32 -0500

    FishList

--- scripts/fishList.js
+++ scripts/fishList.js
@@ -4,8 +4,14 @@ import { database } from './aquariumData.js';
 export const fishList = () => {
     // Generate an HTML representation of each fish
     const fishes = database.fish
+
+    let fishHTML = "<ul>"
     
     for (const fish of fishes) {
-        
+        fishHTML += `<li>${fish.name}</li>`
     }
+
+    fishHTML += "</ul>"
+
+    return fishHTML
 };
