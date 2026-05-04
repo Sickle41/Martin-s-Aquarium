30de3341ec34cd5a7dd6cc88483f50729a23a44d
Sickle41 <dracopest@gmail.com>
2026-05-04 14:10:28 -0500

    Database

---
+++ scripts/aquariumData.js
@@ -0,0 +1,61 @@
+export const database = {
+    fish: [
+        {
+            name: "Nemo",
+            species: "Clownfish",
+            length: 3,
+            location: "Great Barrier Reef",
+            diet: "Omnivore",
+            image: "https://example.com/images/nemo.jpg"
+        },
+        {
+            name: "Dory",
+            species: "Blue Tang",
+            length: 5,
+            location: "Great Barrier Reef",
+            diet: "Omnivore",
+            image: "https://example.com/images/dory.jpg"
+        },
+                {
+            name: "Marlin",
+            species: "Clownfish",
+            length: 4,
+            location: "Great Barrier Reef",
+            diet: "Omnivore",
+            image: "https://example.com/images/marlin.jpg"
+        },
+        {
+            name: "Gill",
+            species: "Moorish Idol",
+            length: 7,
+            location: "Dentist's Fish Tank",
+            diet: "Carnivore",
+            image: "https://example.com/images/gill.jpg"
+        },
+        {
+            name: "Bloat",
+            species: "Pufferfish",
+            length: 6,
+            location: "Dentist's Fish Tank",
+            diet: "Carnivore",
+            image: "https://example.com/images/bloat.jpg"
+        },
+        {
+            name: "Peach",
+            species: "Starfish",
+            length: 2,
+            location: "Dentist's Fish Tank",
+            diet: "Omnivore",
+            image: "https://example.com/images/peach.jpg"
+        },
+        {
+            name: "Deb",
+            species: "Fourstripe Damselfish",
+            length: 3,
+            location: "Dentist's Fish Tank",
+            diet: "Omnivore",
+            image: "https://example.com/images/deb.jpg"
+        }
+
+    ]
+}
