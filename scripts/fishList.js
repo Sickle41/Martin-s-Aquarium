fd1beb13229c9880c1b3a26b196c6d36b6bbbcaf
Sickle41 <dracopest@gmail.com>
2026-05-18 18:18:37 -0500

    fishFilterHTML

--- scripts/fishList.js
+++ scripts/fishList.js
@@ -1,17 +1,38 @@
-import { database } from './aquariumData.js';
+import { mostHolyFish, soldierFish, regularFish } from './aquariumData.js';
 
+const fishConverter = (fishObject) => {
+    const fishHTML = `
+        <section class="fish-card">
+            <img class="fish-card__image" src="${fishObject.image}" alt="${fishObject.name}" />
+            <h3 class="fish-card__name">${fishObject.name}</h3>
+            <p class="fish-card__species">${fishObject.species}</p>
+            <p class="fish-card__length">Length: ${fishObject.length} cm</p>
+            <p class="fish-card__location">Found in: ${fishObject.location}</p>
+            <p class="fish-card__diet">Diet: ${fishObject.diet}</p>
+        </section>
+    `
+    return fishHTML
+}
 
 export const fishList = () => {
-    // Generate an HTML representation of each fish
-    const fishes = database.fish
+    const holyFish = mostHolyFish();
+    const soldiers = soldierFish();
+    const regulars = regularFish();
 
-    let fishHTML = "<ul>"
-    
-    for (const fish of fishes) {
-        fishHTML += `<li>${fish.name}</li>`
+    let allFishHTML = "<h2>Holy Fish</h2>";
+    for (const fish of holyFish) {
+        allFishHTML += fishConverter(fish);
     }
 
-    fishHTML += "</ul>"
+    allFishHTML += "<h2>Soldier Fish</h2>";
+    for (const fish of soldiers) {
+        allFishHTML += fishConverter(fish);
+    }
 
-    return fishHTML
+    allFishHTML += "<h2>Regular Fish</h2>";
+    for (const fish of regulars) {
+        allFishHTML += fishConverter(fish);
+    }
+
+    return allFishHTML;
 };
--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,9 @@
 import { fishList } from './fishList.js'
 import { tipList } from './tipList.js'
 import { locationList } from './locationList.js'
+import { mostHolyFish } from './aquariumData.js'
+import { soldierFish } from './aquariumData.js'
+import { regularFish } from './aquariumData.js'
 
 // Generate the fish list
 const fishHTML = fishList()
